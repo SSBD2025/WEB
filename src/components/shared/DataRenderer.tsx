a2dbb4c5229c3e23e344cd95c776dad21e977fa2
Bartosz Podemski <247766@edu.p.lodz.pl>
2025-04-27 18:49:09 +0200

    Implement data renderer component, create constans files for states and routes

---
+++ src/components/shared/DataRenderer.tsx
@@ -0,0 +1,60 @@
+import { DEFAULT_EMPTY, DEFAULT_ERROR } from "@/constants/states";
+
+interface Props<T> {
+  status: "loading" | "success" | "error";
+  error?: {
+    title: string;
+    details: string;
+  };
+  data: T[] | null | undefined;
+  empty?: {
+    title: string;
+    message: string;
+  };
+  render: (data: T[]) => React.ReactNode;
+}
+
+interface StateSkeletonProps {
+  title: string;
+  message: string;
+}
+
+const StateSkeleton = ({ title, message }: StateSkeletonProps) => (
+  <div className="mt-16 flex w-full flex-col items-center justify-center">
+    <h2 className="text-2xl font-bold leading-8">{title}</h2>
+    <p className="max-w-md text-center text-sm font-normal leading-5">
+      {message}
+    </p>
+  </div>
+);
+
+const LoadingSkeleton = () => (
+  <div className="mt-16 flex w-full justify-center">
+    <span className="loading loading-spinner loading-lg" />
+  </div>
+);
+
+const DataRenderer = <T,>({
+  status,
+  error,
+  data,
+  empty = DEFAULT_EMPTY,
+  render,
+}: Props<T>) => {
+    if (status === "loading") return <LoadingSkeleton />;
+
+  if (status === "error")
+    return (
+      <StateSkeleton
+        title={error?.title || DEFAULT_ERROR.title}
+        message={error?.details || DEFAULT_ERROR.message}
+      />
+    );
+
+  if (!data || data.length === 0)
+    return <StateSkeleton title={empty.title} message={empty.message} />;
+
+  return <div>{render(data)}</div>;
+};
+
+export default DataRenderer;
---
+++ src/constants/states.ts
@@ -0,0 +1,9 @@
+export const DEFAULT_EMPTY = {
+    title: "No Data Found",
+    message: "Looks like we couldn't find any data for you.",
+}
+
+export const DEFAULT_ERROR = {
+    title: "An Error Occurred",
+    message: "Looks like something went wrong. Please try again later.",
+}
