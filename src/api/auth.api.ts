12829a1c1adc027685d4554d676c7ab67b31c6a0
Mr.Cin <247810@edu.p.lodz.pl>
2025-05-19 12:49:12 +0200

    Error handling, admin register done

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -1,11 +1,18 @@
-import authClient from "@/lib/authClient"
-import { RegisterUserRequest } from "@/types/register_user"
+import authClient from "@/lib/authClient";
+import { RegisterUserRequest } from "@/types/register_user";
 
 export const registerUser = async (
-    userType: "client" | "dietician",
+    userType: "client" | "dietician" | "admin",
     body: RegisterUserRequest
 ) => {
-    const endpoint = `/${userType}/register`
-    const res = await authClient.post(endpoint, body)
-    return res.data
-}
+    const endpoint = `/${userType}/register`;
+    const config = userType === "admin"
+        ? {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+        }
+        : {};
+    const res = await authClient.post(endpoint, body, config);
+    return res.data;
+};
