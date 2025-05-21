44baaa15999ce1f4d80a093117d3f1b0ae90406b
Piotr Sokoliński <247790@edu.p.lodz.pl>
2025-05-21 09:55:39 +0000

    search users

--- src/api/account.api.ts
+++ src/api/account.api.ts
@@ -2,6 +2,7 @@ import apiClient from "@/lib/apiClient";
 import { AllAccounts } from "@/types/user";
 
 export interface AccountsQueryParams {
+  searchPhrase?: string;
   page?: number;
   size?: number;
   sortBy?: string;
@@ -9,18 +10,21 @@ export interface AccountsQueryParams {
 }
 
 export const getAllAccounts = async ({
-  page,
-  size,
-  sortBy,
-  sortOrder,
-}: AccountsQueryParams) => {
+       searchPhrase,
+       page,
+       size,
+       sortBy,
+       sortOrder,
+     }: AccountsQueryParams) => {
   const sort = sortBy && sortOrder ? `${sortBy},${sortOrder}` : undefined;
   const res = await apiClient.get("/account", {
     params: {
-      page,
-      size,
+      ...(searchPhrase ? { searchPhrase } : {}),
+      ...(typeof page === "number" ? { page } : {}),
+      ...(typeof size === "number" ? { size } : {}),
       ...(sort ? { sort } : {}),
     },
   });
   return res.data as AllAccounts;
 };
+
