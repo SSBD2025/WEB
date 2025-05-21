6904ff01ac200f7d500113f1dafb413824bd0090
wmichalak <247734@edu.p.lodz.pl>
2025-05-21 16:57:34 +0200

    Code login page

---
+++ src/pages/CodeloginPage.tsx
@@ -0,0 +1,12 @@
+import Codelogin from "@/components/codelogin.tsx";
+
+
+const CodeloginPage = () => {
+    return (
+        <div className="flex items-center justify-center w-full flex-grow">
+            <Codelogin/>
+        </div>
+    );
+}
+
+export default CodeloginPage;
