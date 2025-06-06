50706b92acde6914b04c0f3e570c831578450267
Mr.Cin <247810@edu.p.lodz.pl>
2025-06-06 21:23:05 +0200

    Small fix

--- src/pages/ShowClientReportsByDietician.tsx
+++ src/pages/ShowClientReportsByDietician.tsx
@@ -3,10 +3,7 @@ import BloodTestReports from "@/components/blood-test-reports"
 export default function Page() {
     return (
         <div className="flex-grow p-4">
-            <BloodTestReports userRole={"dietician"}
-                onEditReport={(reportId) => {
-                    console.log("TODO, reportId: " + reportId)
-            }} />
+            <BloodTestReports userRole={"dietician"}/>
         </div>
     )
 }
